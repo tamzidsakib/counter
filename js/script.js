8f9ddb23ca99964782a0d218aae44fbc7f3bd1af
Tamzid Sakib <93206260+tamzidsakib@users.noreply.github.com>
2022-03-04 20:02:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// JS
+
+
+
+
+
+// jq
+
+$(document).ready(function(){
+
+// counter github
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+// jscounter
+$(function(){
+  $('.count-num').rCounter({
+    duration: 30
+  });
+});
+
+});
